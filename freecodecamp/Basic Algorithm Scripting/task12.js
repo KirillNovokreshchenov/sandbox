0cc160747ebab18d46650f261799aa998ee93c6c
KirillNovokreshchenov <kirillnovokrest@gmail.com>
2023-04-12 15:40:34 +0300

    FCC

---
+++ freecodecamp/Basic Algorithm Scripting/task12.js
@@ -0,0 +1,8 @@
+function frankenSplice(arr1, arr2, n) {
+    const newArr = [...arr2]
+    newArr.splice(n, 0, ...arr1)
+    return newArr
+    
+  }
+  console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
+  frankenSplice([1, 2, 3], [4, 5, 6], 1);
